f775345a0d97a2593b4be5a098438a8cc94b9712
nshervt <navidst@colorado.edu>
2022-01-27 13:32:23 -0500

    ENH: loading model

--- load_model_backup.py
+++ load_model_backup.py
@@ -5,32 +5,19 @@ import torch
 class NewModel(nn.Module):
     def __init__(self):
         super(NewModel, self).__init__()
-        self.cn1 = nn.Conv2d(1, 256, kernel_size=3, stride=2)
-        self.cn2 = nn.Conv2d(256, 256, kernel_size=3, stride=1)
-        self.cn3 = nn.Conv2d(256, 256, kernel_size=3, stride=2)
-        self.cn4 = nn.Conv2d(256, 256, kernel_size=3, stride=1)
-        self.cn5 = nn.Conv2d(256, 256, kernel_size=3, stride=2)
-        self.cn6 = nn.Conv2d(256, 256, kernel_size=3, stride=2)
+        self.cn1 = nn.Conv2d(1, 16, kernel_size=7, stride=2)
+        self.cn2 = nn.Conv2d(16, 32, kernel_size=4, stride=1)
+        self.cn3 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
 
         self.relu = nn.ReLU()
 
-    def forward(self, x):
-        y1 = self.relu(self.cn1(x))
-        y2 = self.relu(self.cn2(y1))
-        y3 = self.relu(self.cn3(y2))
-        y4 = self.relu(self.cn4(y3))
-        y5 = self.relu(self.cn5(y4))
-        y6 = self.relu(self.cn6(y5))
 
-        return y6.view(y6.size(0), -1)
-
-
-path = '/Users/nshervt/Library/Application Support/JetBrains/PyCharm2021.2/scratches/data/models'
+path = '/Users/nshervt/Private/Research/PostDoc/MetaPlasticity/Codes/MetaPlasticity/data/models/omniglot_example'
 stat = True
-saving = False
+saving = True
 if saving:
     # -- load model
-    path_model = '/Users/nshervt/Private/Research/MetaPlasticity/Codes/mrcl/saved_models/1_7/learner.model'
+    path_model = '/Users/nshervt/Private/Research/PostDoc/MetaPlasticity/Codes/mrcl-master/results/27January2022/OML_Omniglot/0/1_1/learner.model'
     old_model = torch.load(path_model, map_location='cpu')
 
     new_model = NewModel()
@@ -48,9 +35,3 @@ else:
         old_model = torch.load(path + '/model.pth')
     else:
         old_model = torch.load(path + '/model_stat.pth')
-
-
-new_model = NewModel()
-
-for idx, (old_key, (new_key, new_param)) in enumerate(zip(old_model, new_model.named_parameters())):
-    new_param.data = old_model[new_key]
